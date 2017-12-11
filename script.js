d467cea468236be0af2e02da27540318b07eb4b8
pranay <reddy.pranay03@gmail.com>
2017-12-11 16:36:22 -0600

    Added Collision Detection

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ setInterval(draw,10);
 
 var x = canvas.width/2;
 var y = canvas.height - 30;
+var ballRadius = 10;
 var dx = 2;
 var dy = -2;
 
@@ -12,6 +13,16 @@ function draw()
 {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
+
+    if( y + dy < 0 || y + dy > canvas.height)
+    {
+        dy = -dy;
+    }
+    if( x + dx < 0 || x + dx > canvas.width)
+    {
+        dx = -dx;
+    }
+
     x += dx;
     y +=dy;
 }
@@ -19,7 +30,7 @@ function draw()
 function drawBall()
 {
     ctx.beginPath();
-    ctx.arc(x,y,10,0, Math.PI*2);
+    ctx.arc(x,y,ballRadius,0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
