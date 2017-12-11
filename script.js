0ea1cc701d96abf922a0bf923ad103ad83cde843
pranay <reddy.pranay03@gmail.com>
2017-12-11 16:22:46 -0600

    Ball Drawn

---
+++ script.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+setInterval(draw,10);
+
+var x = canvas.width/2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function draw()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawBall();
+    x += dx;
+    y +=dy;
+}
+
+function drawBall()
+{
+    ctx.beginPath();
+    ctx.arc(x,y,10,0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
