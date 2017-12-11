9bb7812aa720787dc9506a2d861f7c73ba777d04
pranay <reddy.pranay03@gmail.com>
2017-12-11 17:17:37 -0600

    created a paddle and gave it functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-setInterval(draw,10);
+
 
 var x = canvas.width/2;
 var y = canvas.height - 30;
@@ -9,10 +9,22 @@ var ballRadius = 10;
 var dx = 2;
 var dy = -2;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth)/2;
+var paddleY = (canvas.height - paddleHeight);
+
+var leftPressed = false;
+var rightPressed = false;
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
 function draw()
 {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
+    drawPaddle();
 
     if( y + dy < ballRadius || y + dy > canvas.height-ballRadius)
     {
@@ -21,6 +33,15 @@ function draw()
     if( x + dx < ballRadius || x + dx > canvas.width-ballRadius)
     {
         dx = -dx;
+    }
+
+    if(leftPressed && paddleX > 0)
+    {
+        paddleX -= 7;
+    }
+    else if(rightPressed && paddleX < canvas.width - paddleWidth)
+    {
+        paddleX += 7;
     }
 
     x += dx;
@@ -35,3 +56,41 @@ function drawBall()
     ctx.fill();
     ctx.closePath();
 }
+
+function drawPaddle()
+{
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+
+function keyDownHandler(e)
+{
+    if(e.keyCode == 37)
+    {
+        console.log('Working');
+        leftPressed = true;
+    }
+    else if(e.keyCode == 39)
+    {
+        rightPressed = true;
+    }
+}
+
+function keyUpHandler(e)
+{
+    if(e.keyCode == 37)
+    {
+        leftPressed = false;
+    }
+    else if(e.keyCode == 39)
+    {
+        rightPressed = false;
+    }
+}
+
+setInterval(draw,10);
